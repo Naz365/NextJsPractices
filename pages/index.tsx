9d97ad3672137e61d7e6fb016c83016da67c2aff
Naz365 <nazmul.gub.cse@gmail.com>
2022-04-02 02:36:10 +0600

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from "react"
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+  <>
+  <nav className="">
+    <div className="navbar bg-red-600 flex  justify-evenly .. ">
+        <div className=" ">
+            <a className="btn btn-ghost ">
+                  <img className=" h-14 w-15 " src="https://i.ibb.co/5F8GX3P/275695272-306152975005517-7744725845089821097-n.png" alt=""  />
+             </a>
+        </div>
+        <div className="flex items-center justify-items-center justify-around">
+          <div className="text-slate-100 ">
+            Home  |
+          </div>
+            <div className="ml-4 w-10 flex">
+              <img className="w-10 h-10 p-1 rounded-full ring-2 ring-neutral-300 light:ring-gray-500" src="https://api.lorem.space/image/face?hash=33791" />
+              <span className="text-slate-100 ml-2 underline font-light">Jasim Uddin</span>
+
+            </div>
+        </div>
+  
+    </div>
+
+  </nav>
+
+  <main>
+    
+  </main>
+  </>
 )
 
 export default IndexPage
