8dc9089ae3679a094fa2889a4eebc8e98df16fbe
Naz365 <nazmul.gub.cse@gmail.com>
2022-04-10 02:03:50 +0600

    navber update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,67 @@
-import React from "react"
+import React from "react";
 
 const IndexPage = () => (
   <>
-  <nav className="">
-    <div className="navbar bg-red-600 flex  justify-evenly .. ">
-        <div className=" ">
-            <a className="btn btn-ghost ">
-                  <img className=" h-14 w-15 " src="https://i.ibb.co/5F8GX3P/275695272-306152975005517-7744725845089821097-n.png" alt=""  />
-             </a>
-        </div>
-        <div className="flex items-center justify-items-center justify-around">
-          <div className="text-slate-100 ">
-            Home  |
-          </div>
-            <div className="ml-4 w-10 flex">
-              <img className="w-10 h-10 p-1 rounded-full ring-2 ring-neutral-300 light:ring-gray-500" src="https://api.lorem.space/image/face?hash=33791" />
-              <span className="text-slate-100 ml-2 underline font-light">Jasim Uddin</span>
-
-            </div>
-        </div>
-  
-    </div>
-
-  </nav>
-
-  <main>
+    <nav
+      className="relative flex flex-wrap items-center justify-between 
     
-  </main>
+     bg-red-500
+     "
+    >
+      <div className="container px-2 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+          <a
+            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+            href="#pablo"
+          >
+            <img
+              className=" h-14 w-15 "
+              src="https://i.ibb.co/5F8GX3P/275695272-306152975005517-7744725845089821097-n.png"
+              alt=""
+            />
+          </a>
+          <button
+            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+          >
+            <span className="block relative w-6 h-px rounded-sm bg-white"></span>
+            <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+            <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+          </button>
+        </div>
+        <div
+          className="lg:flex flex-grow items-center hidden justify-between w-full md:flex md:w-auto md:order-1"
+          id="example-navbar-warning"
+        >
+          <ul className="flex flex-col lg:flex-row list-none ml-auto mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <li className="nav-item">
+              <a
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                href="#pablo"
+              >
+                <span className="ml-2 pt-4">Home | </span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                href="#pablo"
+              >
+                <img
+                  className="w-10 h-10 p-1 rounded-full ring-2 ring-neutral-300 light:ring-gray-500"
+                  src="https://api.lorem.space/image/face?hash=33791"
+                />
+                <span className="text-slate-100 ml-2 underline pt-2">
+                  Jasim Uddin
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
   </>
-)
+);
 
-export default IndexPage
+export default IndexPage;
---
+++ pages/test.ts
@@ -0,0 +1,55 @@
+<nav className="">
+    <div className="navbar bg-red-600 flex  justify-evenly .. ">
+        <div className=" ">
+            <a className="btn btn-ghost ">
+                  <img className=" h-14 w-15 " src="https://i.ibb.co/5F8GX3P/275695272-306152975005517-7744725845089821097-n.png" alt=""  />
+             </a>
+        </div>
+        <div className="flex items-center justify-items-center justify-around">
+          <div className="text-slate-100 ">
+            Home  |
+          </div>
+            <div className="ml-4 w-10 flex">
+              <img className="w-10 h-10 p-1 rounded-full ring-2 ring-neutral-300 light:ring-gray-500" src="https://api.lorem.space/image/face?hash=33791" />
+              <span className="text-slate-100 ml-2 underline font-light">Jasim Uddin</span>
+
+            </div>
+        </div>
+  
+    </div>
+
+  </nav>
+
+  <main className="">
+    <section  className="my-20 ">
+       {/* first hero section */}
+
+  <div className="bg-red-200 flex w-3/4 h-40 mx-auto justify-items-center items-center">
+        <div className="mx-5  border-r-2 border-black border-dotted">
+           <img src="https://i.ibb.co/5F8GX3P/275695272-306152975005517-7744725845089821097-n.png" className="h-20 w-22 mx-5 p-3" />     
+        </div>
+
+        <div>
+          <h1 className="text-4xl font-bold">Jazeerat Dubai Visa Application from</h1>                    
+        </div>
+  </div>
+    </section>
+  {/* first hero section end */}
+
+{/* second hero section */}
+  <section>
+<div>
+  <div>
+    <img src="" />
+    <img src="" />
+  </div>
+  <div>
+    <div>
+      <img src="" alt=""  />
+      <span>Jasim Uddin</span>
+    </div>
+  </div>
+</div>
+  </section>
+
+  </main>
